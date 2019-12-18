78b7a6c828f5a761b86ff3a364a829e6f2e59fd8
Alexander Slesarenko <avslesarenko@gmail.com>
2019-12-18 13:18:04 +0300

    removing scala-examples from gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,6 @@ repositories {
 dependencies {
   // the root project doesn't have Java code, but this dependency is required for fat jar generation
   implementation(project(":java-examples"))
-  implementation(project(":scala-examples"))
 }
 
 java {

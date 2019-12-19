cd1e4c33c43fc280d03e243e8ab5ddfd7e3677d8
Alexander Slesarenko <avslesarenko@gmail.com>
2019-12-19 10:41:46 +0300

    CheckAddressCmd specs

--- scala-examples/src/main/scala/org/ergoplatform/appkit/examples/ergotool/CheckAddressCmd.scala
+++ scala-examples/src/main/scala/org/ergoplatform/appkit/examples/ergotool/CheckAddressCmd.scala
@@ -4,9 +4,9 @@ import org.ergoplatform.appkit.config.ErgoToolConfig
 import org.ergoplatform.appkit.{NetworkType, Address}
 import org.ergoplatform.appkit.console.Console
 
-case class CheckAddressCmd(toolConf: ErgoToolConfig, name: String, network: NetworkType, mnemonic: String, mnemonicPass: String, address: String) extends Cmd {
+case class CheckAddressCmd(toolConf: ErgoToolConfig, name: String, network: NetworkType, mnemonic: String, mnemonicPass: Array[Char], address: Address) extends Cmd {
   override def run(console: Console): Unit = {
-    val addressComputed = Address.fromMnemonic(network, mnemonic, mnemonicPass).toString
+    val addressComputed = Address.fromMnemonic(network, mnemonic, String.valueOf(mnemonicPass))
     val res = if (addressComputed == address) "Ok"
               else s"$addressComputed != $address"
     console.print(res)
@@ -14,20 +14,21 @@ case class CheckAddressCmd(toolConf: ErgoToolConfig, name: String, network: Netw
 }
 
 object CheckAddressCmd extends CmdFactory(
-  name = "checkAddress", cmdParamSyntax = "testnet|mainnet <mnemonic> <mnemonic password> <address>",
+  name = "checkAddress", cmdParamSyntax = "testnet|mainnet <mnemonic> <address>",
   description = "Check the given mnemonic and password pair correspond to the given address") {
 
   override def parseCmd(args: Seq[String], toolConf: ErgoToolConfig, console: Console): Cmd = {
-    if (args.length < 5) error("not enough parameters")
-    val network = args(1)
+    val network = if (args.length > 1) args(1) else error("network type is not specified")
     val networkType = network match {
       case "testnet" => NetworkType.TESTNET
       case "mainnet" => NetworkType.MAINNET
       case _ => error(s"Invalid network type $network")
     }
-    val mnemonic = args(2)
-    val mnemonicPass = args(3)
-    val address = args(4)
+    val mnemonic = if (args.length > 2) args(2) else error("mnemonic is not specified")
+    val address = Address.create(if (args.length > 3) args(3) else error("address is not specified"))
+    if (networkType != address.getNetworkType)
+      error(s"Network type of the address ${address.getNetworkType} don't match expected $networkType")
+    val mnemonicPass = console.readPassword("Mnemonic password> ")
     CheckAddressCmd(toolConf, name, networkType, mnemonic, mnemonicPass, address)
   }
 }
--- scala-examples/src/test/scala/org/ergoplatform/appkit/examples/ErgoToolSpec.scala
+++ scala-examples/src/test/scala/org/ergoplatform/appkit/examples/ErgoToolSpec.scala
@@ -1,11 +1,10 @@
 package org.ergoplatform.appkit.examples
 
-import java.io.{ByteArrayOutputStream, PrintStream}
-
 import org.ergoplatform.appkit.examples.ergotool.ErgoTool
 import org.scalatest.{PropSpec, Matchers}
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
+/** To run in IDEA set `Working directory` in Run/Debug configuration. */
 class ErgoToolSpec extends PropSpec with Matchers with ScalaCheckDrivenPropertyChecks with ConsoleTesting {
   val addrStr = "3WzR39tWQ5cxxWWX6ys7wNdJKLijPeyaKgx72uqg9FJRBCdZPovL"
   val mnemonic = "slow silly start wash bundle suffer bulb ancient height spin express remind today effort helmet"
@@ -43,9 +42,35 @@ class ErgoToolSpec extends PropSpec with Matchers with ScalaCheckDrivenPropertyC
   }
 
   property("address command") {
-    val res = testCommand(
-      s"Mnemonic password> ::;\n$addrStr::;\n",
+    testCommand(
+      s"""Mnemonic password> ::;
+         |$addrStr::;
+         |""".stripMargin,
       "address", "testnet", mnemonic)
+  }
+
+  property("checkAddress command") {
+    testCommand(
+      s"""Mnemonic password> ::;
+         |Ok::;
+         |""".stripMargin,
+      "checkAddress", "testnet", mnemonic, addrStr)
+  }
+
+  property("checkAddress command validates address format") {
+    val res = runCommand(
+      s"""Mnemonic password> ::;
+        |""".stripMargin,
+      "checkAddress", "testnet", mnemonic, "someaddress")
+    res.contains("Invalid address encoding, expected base58 string: someaddress") shouldBe true
+  }
+
+  property("checkAddress command validates network type") {
+    val res = runCommand(
+      s"""Mnemonic password> ::;
+        |""".stripMargin,
+      "checkAddress", "testnet", mnemonic, "9f4QF8AD1nQ3nJahQVkMj8hFSVVzVom77b52JU7EW71Zexg6N8v")
+    res.contains("Network type of the address MAINNET don't match expected TESTNET") shouldBe true
   }
 
 }
